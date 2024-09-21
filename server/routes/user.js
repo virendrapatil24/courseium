7a7e3fad6b82266814121a279ec0e2f2f2fee40a
Virendra Patil <virendrapatil.24vp@gmail.com>
2024-09-22 01:54:24 +0530

    adds api skeletons

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,4 +2,19 @@ import { Router } from "express";
 
 const userRouter = Router();
 
+// sign up user
+userRouter.post("/signup", (res, req) => {});
+
+// login user
+userRouter.post("/login", (res, req) => {});
+
+// list user created courses
+userRouter.get("/my-courses", (res, req) => {});
+
+// list user purchased courses
+userRouter.get("/my-learnings", (res, req) => {});
+
+// list purchases (just the transactions)
+userRouter.get("/purchases", (res, req) => {});
+
 export default userRouter;
